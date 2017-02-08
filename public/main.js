f0aa6b60086b5775905369bc9798782912ee9e11
Anuj Shankar <anujshankar95@gmail.com>
2017-02-08 15:56:12 +0530

    Back State

--- public/main.js
+++ public/main.js
@@ -1,3 +1,7 @@
-const getRead = require('./readDB')
-
-getRead()
+const list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+    
+  }
+}, false);
--- public/readDB.js
+++ public/readDB.js
@@ -3,7 +3,7 @@ function read() {
     let result = ''
     console.log(data)
     for (let iter = 0; iter < data.length; iter++) {
-      result += `<li>${data[iter].description}<button id=${data[iter].id}>delete</button></li>` // Prone to XSS
+      result += `<li>${data[iter].description}<button class='close' id=${data[iter].id}>x</button></li>` // Prone to XSS
     }
     $("#result").html((result))
     $('button').click(function () {
@@ -22,3 +22,10 @@ $(document).ready(function () {
   read()
 })
 
+`<li class="list-group-item" >
+            <div class = "row-fluid">
+              <input class = "note-check checkbox checkbox-primary" noteId = "${id}" type="checkbox" ${status === true ? "checked" : ''} > 
+              <input noteId = "${id}" class = "col-sm-8 note-holder" value = "${description}" /> 
+              <span noteId=${id} class = "deleteButton col-sm-1">:x:</span> 
+            </div>
+</li>`;
