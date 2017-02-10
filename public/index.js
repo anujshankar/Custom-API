d12299f61a0ba99e13fe5a3f1be764d383c8ce8e
Anuj Shankar <anujshankar95@gmail.com>
2017-02-10 17:54:21 +0530

    item count added

--- public/index.js
+++ public/index.js
@@ -1,13 +1,18 @@
 // escape data for avoiding XSS attacks.
-/*eslint-disable*/
+
+let allTodos = []
+let activeTodos = []
+let completedTodos = []
 
 function read() {
   $.get("/read", function (data) {
+    allTodos = data
+    console.log(allTodos)
     let result = ''
-    for (let iter = 0; iter < data.length; iter++) {
-      const id = data[iter].id
-      const description = data[iter].description
-      const status = data[iter].status
+    for (let i = 0; i < data.length; i++) {
+      const id = data[i].id
+      const description = data[i].description
+      const status = data[i].status
 
       let textbox = `<input
        id="text-${id}"
@@ -27,16 +32,42 @@ function read() {
                   </div>
                   </li>`
     }
-    $("#result").html((result))
+    //Div append tab wrapper
+    const itemsLeftCount = getItemsLeftCount(allTodos)
+    const itemsLeft = `<span id="items-left">${itemsLeftCount} items left</span>`
+    const allButton = `<button id="all-button">All</button>`
+    const activeButton = `<button id="active-button">Active</button>`
+    const completedButton = `<button id="completed-button">Completed</button>`
+    const tabWrapper = `${itemsLeft}${allButton}${activeButton}${completedButton}`
+    const ulTag = document.getElementById('result')
+    ulTag.innerHTML = result
+    const divTabWrapper = document.getElementById('tab-wrapper')
+    divTabWrapper.innerHTML = tabWrapper
   })
+}
+
+function getItemsLeftCount(todos) {
+  let itemsLeftCount = 0
+  activeTodos = []
+  completedTodos = []
+  for (let i = 0; i < todos.length; i++) {
+    if (!todos[i].status) {
+      itemsLeftCount++
+      activeTodos.push(todos[i])
+    } else {
+      completedTodos.push(todos[i])
+    }
+  }
+  return itemsLeftCount
 }
 
 function strikeThrough(id) {
   const queryId = id.split('-')[1]
-  const textboxId = 'text-' + queryId 
-  const checkId =  'check-' + queryId
+  const textboxId = 'text-' + queryId
+  const checkId = 'check-' + queryId
   const textElement = document.getElementById(textboxId)
   const checkElement = document.getElementById(checkId)
+  let status
   if (checkElement.checked) {
     textElement.style.textDecoration = "line-through";
     status = true
@@ -50,10 +81,21 @@ function strikeThrough(id) {
 function updateStatus(id) {
   const queryId = id.split('-')[1]
   const status = strikeThrough(id)
+
   $.ajax({
     data: `status=${status}`,
     url: `/update/${queryId}`,
     type: 'PUT',
+    success: () => {
+      for (let i = 0; i < allTodos.length; i++) {
+        if (allTodos[i].id === Number(queryId)) {
+          allTodos[i].status = status
+          break
+        }
+      }
+      const spanItemsLeft = document.getElementById('items-left')
+      spanItemsLeft.innerHTML = getItemsLeftCount(allTodos) + ' items left'
+    }
   })
 }
 
@@ -95,7 +137,15 @@ $("#writeForm").submit((e) => {
                   <button id="button-${id}" onClick="deleteElement(this.id)" class = "deleteButton">x</button> 
                   </div>
                   </li>`
-    $("#result").append(taskData);
+    $("#result").append(taskData)
+    allTodos.push(
+      {
+        id: data.id,
+        description: task,
+        status: false
+      })
+    const spanItemsLeft = document.getElementById('items-left')
+    spanItemsLeft.innerHTML = getItemsLeftCount(allTodos) + ' items left'
   })
 })
 
